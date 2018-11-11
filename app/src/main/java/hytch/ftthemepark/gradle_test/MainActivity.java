5de6ec82a53b48a1fc4f4ab8c56d97e3fc5d6299
hanjiahu <hanjiahu@saicmobility.com>
2018-11-11 18:06:08 +0800

    init

---
+++ app/src/main/java/hytch/ftthemepark/gradle_test/MainActivity.java
@@ -0,0 +1,13 @@
+package hytch.ftthemepark.gradle_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
